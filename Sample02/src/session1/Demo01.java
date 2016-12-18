afd3cc51d91dfc6792901f34a7714f06ef85d1d2
Jay Siva <adhidhan@gmail.com>
2016-12-18 22:35:37 +0530

    testing in main git

--- Sample02/src/session1/Demo01.java
+++ Sample02/src/session1/Demo01.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("WElcome to GitHub");
 	System.out.println("Share your project with github");
 	
+	System.out.println("Testing my github one");
 }
 }
