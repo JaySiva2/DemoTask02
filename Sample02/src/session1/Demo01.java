bf5f4784d18af3ab37be654339af3c6d5a786621
Jay Siva <adhidhan@gmail.com>
2016-12-18 22:11:51 +0530

    Usage about the file

--- Sample02/src/session1/Demo01.java
+++ Sample02/src/session1/Demo01.java
@@ -3,6 +3,7 @@ package session1;
 public class Demo01 {
 public static void main(String[] args) {
 	System.out.println("WElcome to GitHub");
+	System.out.println("Share your project with github");
 	
 }
 }
